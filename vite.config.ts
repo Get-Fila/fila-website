4a4698871c26fa5658492fb02b31b192abc5ca07
Sylvana Santos <sylvana@getfila.com>
2026-08-18 08:46:32 -0700

    feat: privacy policy

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: parseInt(process.env.PORT || '5174'),
+    port: parseInt(process.env.PORT || "5174"),
   },
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        privacyPolicy: resolve(__dirname, "privacy-policy/index.html"),
+      },
+    },
+  },
+});
